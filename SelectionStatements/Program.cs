75bddfca17b161db7c192543b9be87ffff7856a7
Devan <deshlock010101@hotmai.com>
2022-02-14 15:20:13 -0600

    finished the assignment.

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,24 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            
+            Console.Write("What is your guess for the number?: ");           
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber < userInput)
+            {
+                Console.WriteLine("You have guessed to high. ");
+            }
+            else if (favNumber > userInput)
+            {
+                Console.WriteLine("You have guessed to low. ");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations. ");
+            }
         }
     }
 }
